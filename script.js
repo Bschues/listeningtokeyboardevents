3781eb072f4f41b3e83ed21e90b550b32f1e81f7
bschue <bschuess3@gmail.com>
2018-05-14 17:42:37 -0400

    the box moves

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-const boxTop = 200;
-const boxLeft = 200;
+let boxTop = 200;
+let boxLeft = 200;
 'use strict';
 
 document.addEventListener('keydown', (event) => {
-     const keyName = event.key;
-    console.log('keydown event\n\n' + 'key: ' + keyName);
-    document.getElementById("box").style.top = boxtop + "px";
+    const keyName = event.key;
+    if (keyName == "ArrowDown") {
+        // console.log("ArrowDown");
+        boxTop = boxTop + 10;
+        document.getElementById("box").style.top = boxTop + "px";
+    }
+    else if (keyName == "ArrowUp") {
+        // console.log("ArrowUp");
+        boxTop = boxTop - 10;
+        document.getElementById("box").style.top = boxTop + "px";
+    }
+    else if (keyName == "ArrowRight") {
+        // console.log("ArrowRight");
+        boxLeft = boxLeft + 10;
+        document.getElementById("box").style.left = boxTop + "px";
+    }
+    else if (keyName == "ArrowLeft") {
+        // console.log("ArrowLeft");
+        boxLeft = boxLeft - 10;
+        document.getElementById("box").style.left = boxTop + "px";
+    }
 });
 //document.getElementById("box").style.top = boxtop + "px";
