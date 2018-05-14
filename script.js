52ef0f9bae233f0cfc97bbf27c1c9b803ac4e6a2
bschue <bschuess3@gmail.com>
2018-05-14 19:52:30 -0400

    obama basketball

--- script.js
+++ script.js
@@ -7,22 +7,22 @@ document.addEventListener('keydown', (event) => {
     const keyName = event.key;
     if (keyName == "ArrowDown") {
         // console.log("ArrowDown");
-        boxTop = boxTop + 10;
+        boxTop = boxTop + 25;
         document.getElementById("box").style.top = boxTop + "px";
     };
     if (keyName == "ArrowUp") {
         // console.log("ArrowUp");
-        boxTop = boxTop - 10;
+        boxTop = boxTop - 25;
         document.getElementById("box").style.top = boxTop + "px";
     };
     if (keyName == "ArrowRight") {
         // console.log("ArrowRight");
-        boxLeft = boxLeft + 10;
+        boxLeft = boxLeft + 25;
         document.getElementById("box").style.left = boxLeft + "px";
     };
     if (keyName == "ArrowLeft") {
         // console.log("ArrowLeft");
-        boxLeft = boxLeft - 10;
+        boxLeft = boxLeft - 25;
         document.getElementById("box").style.left = boxLeft + "px";
     };
 });
