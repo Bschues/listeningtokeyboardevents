52159e8ddc7dd7e4d197a29690a1f05b23af3de1
bschue <bschuess3@gmail.com>
2018-05-15 09:49:08 -0400

    basketball is my favorite sport

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 'use strict';
-let boxTop = 200;
-let boxLeft = 200;
+let boxTop = 585;
+let boxLeft = 20;
 
 
 document.addEventListener('keydown', (event) => {
     const keyName = event.key;
     if (keyName == "ArrowDown") {
         // console.log("ArrowDown");
-        boxTop = boxTop + 25;
+        boxTop = boxTop + 15;
         document.getElementById("box").style.top = boxTop + "px";
     };
     if (keyName == "ArrowUp") {
         // console.log("ArrowUp");
-        boxTop = boxTop - 25;
+        boxTop = boxTop - 15;
         document.getElementById("box").style.top = boxTop + "px";
     };
     if (keyName == "ArrowRight") {
         // console.log("ArrowRight");
-        boxLeft = boxLeft + 25;
+        boxLeft = boxLeft + 15;
         document.getElementById("box").style.left = boxLeft + "px";
     };
     if (keyName == "ArrowLeft") {
         // console.log("ArrowLeft");
-        boxLeft = boxLeft - 25;
+        boxLeft = boxLeft - 15;
         document.getElementById("box").style.left = boxLeft + "px";
     };
 });
